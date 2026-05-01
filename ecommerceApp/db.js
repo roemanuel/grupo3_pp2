1ea7603961deb7be834fe532dce33ec42186fbbe
Grace Vargas <gracielavargasg@gmail.com>
2026-04-30 21:26:29 -0300

    agrego endpoints para producto

---
+++ ecommerceApp/db.js
@@ -0,0 +1,44 @@
+const productosDb = [
+    {
+        id_producto: 1,
+        nombre: "Cafetera Nescafé 230v Blanca Genio S Blanco",
+        precio: 179.999,
+        stock: 5,
+        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTeHC20VN7reUOzJiEHRINl57sjdZEEf1yeaGAoRRqIvTvXYkfCoqcd8a1Lq7rztZI48EpVib6d-XX0nxB_ZJkgP5u4BbI4cJxe2MkwTx0Ad7UVU4yT8kyQN4b-hz0rEQKeTWv8WXuC&usqp=CAc",
+        descuento: true
+    },
+    {
+        id_producto: 2,
+        nombre: "Ventilador Retractil De Techo 4 aspas Color Blanco",
+        precio: 113.149,
+        stock: 50,
+        img: "https://static.hendel.com/media/catalog/product/cache/b866fd8d147dcce474dc8744e477ca66/4/7/47281-min.jpg",
+        descuento: false
+    },
+    {
+        id_producto: 3,
+        nombre: "Perfume Liquid Brun French Avenue 100ml Edp Arabe",
+        precio: 82.081,
+        stock: 100,
+        img: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSa-KyJF7luQhelyspiurmcC6Km3XUmYKlY8yZ1Kgtm2keeLBd4t2JpCOaBVoc3qhGcWwXFRSOGG-kkxVxCkOwjsAwBL0e-opeCKk-Kc8AMpRdrDdNKqK2Wt-BcknTTlp3pXzx0iqQ&usqp=CAc",
+        descuento: false
+    },
+    {
+        id_producto: 4,
+        nombre: "Samsung Galaxy A16 4g 128gb 4 Gb Ram Negro",
+        precio: 257.699,
+        stock: 150,
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTvLAwLAUWKmxIvOkhna6b9oxGfDlTeHTLDZ_pZf6QlFjcSf7ysOKjOt7NxxefKfzMecHXnJ8FqL9LIOT4WIEniqwO6bnToPe4IPjIA_MCgBHhvmsN0R7K5sUMH0PzJ1TNAeC6JhrU&usqp=CAc",
+        descuento: true
+    },
+    {
+        id_producto: 5,
+        nombre: "Colchón KL-Eterna Känn Livet 2 Plazas",
+        precio: 308.999,
+        stock: 200,
+        img: "https://lacardeuse.vtexassets.com/arquivos/id_productos/1581280-800-auto?v=639034040604200000&width=800&height=auto&aspect=true",
+        descuento: false
+    }
+]
+
+export default productosDb;
